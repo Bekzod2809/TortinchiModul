6460c9eec9665089536b9e75e6cc1fc2e5bc947e
Bekzod <bekzodjonxasanov7@gmail.com>
2026-03-12 14:12:21 +0500

    added

---
+++ dars1/dars1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace dars1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //try
+        //{
+        //    string name = "";
+        //    if (name == string.Empty)
+        //    {
+        //        throw new Exception("Hatolik");
+        //    }
+        //}
+        //catch (Exception ex)
+        //{
+        //    Console.WriteLine("hatolik");
+        //}
+        //finally
+        //{
+        //    Console.WriteLine("Operation finished");
+        //}
+
+        //try
+        //{
+        //    var email = "djisd";
+        //    if (!email.Contains("@"))
+        //    {
+        //        throw new Exception("Hatolik");
+        //    }
+        //}
+        //catch (Exception)
+        //{
+
+        //    Console.WriteLine("hatolik");
+        //}
+
+        //try
+        //{
+        //    List<string> students = new List<string>() { "Ali", "Vali", "Hasan" };
+        //    string name = "Alixon";
+        //    if (!students.Contains(name))
+        //    {
+        //        throw new Exception("Hatolik");
+        //    }
+        //}
+        //catch (Exception)
+        //{
+        //    Console.WriteLine(404);
+        //}    
+
+
+    }
+
+}
